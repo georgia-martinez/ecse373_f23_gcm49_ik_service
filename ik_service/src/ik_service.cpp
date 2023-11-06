39bb2809ee6e4226db0199dc6d75c0fea8c9b566
Georgia Martinez <gcm49@case.edu>
2023-11-06 16:54:58 +0000

    Started ik_service

---
+++ ik_service/src/ik_service.cpp
@@ -0,0 +1,23 @@
+#include "ros/ros.h"
+#include "ik_service/PoseIK.h"
+#include "ur_kinematics/ur_kin.h"
+
+// Service function
+bool pose_ik(ik_service::PoseIK::Request &req, ik_service::PoseIK::Response &res) {
+	ROS_INFO("Pose Information...");
+	return true;
+}
+
+int main (int argc, char **argv) {
+
+	ros::init(argc, argv, "ik_service");
+    ros::NodeHandle n;
+
+	// Service node
+    ros::ServiceServer service = n.advertiseService("pose_ik", pose_ik);
+
+    ros::spin();
+
+    return 0;
+
+}
