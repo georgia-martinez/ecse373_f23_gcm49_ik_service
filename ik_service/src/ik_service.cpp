e1b3375afc0f0a3edfa9a7d8501c6b871fe30751
Georgia Martinez <gcm49@case.edu>
2023-12-04 21:22:32 +0000

    Fixed ik_service

--- ik_service/src/ik_service.cpp
+++ ik_service/src/ik_service.cpp
@@ -6,7 +6,7 @@
 bool pose_ik(ik_service::PoseIK::Request &req, ik_service::PoseIK::Response &res) {
 	ROS_INFO("pose_ik service called");
 	
-	float q_sols[8][6];
+	double q_sols[8][6];
 	
     double T[4][4] = {
     	{0.0, -1.0, 0.0, req.part_pose.position.x}, 
@@ -16,6 +16,12 @@ bool pose_ik(ik_service::PoseIK::Request &req, ik_service::PoseIK::Response &res
 	};
 
 	res.num_sols = ur_kinematics::inverse(&T[0][0], (double *) &q_sols[0][0], 0.0);	
+
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 6; j++) {
+			res.joint_solutions[i].joint_angles[j] = q_sols[i][j];
+		}
+	}
 
 	return true;
 }
